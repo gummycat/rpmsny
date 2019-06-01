15a67c63f06d5f4dbfc1ef71887512b5d9273780
KraheZ4Code <sllkrahe@gmail.com>
2019-06-01 16:42:16 -0400

    SLK: Data for Events

--- lib/src/models/host.dart
+++ lib/src/models/host.dart
@@ -1,11 +1,11 @@
 class Host {
 
-  final String hostKey;
+  //final String hostKey;
   final String displayName;
-  final List<String> skills;
-  final int minutesPerSession;
+  final String skills;
+  //final int minutesPerSession;
   //PostMVP final int maxMinutesession;
 
-  Host(this.hostKey, this.displayName, this.skills, this.minutesPerSession);
+  Host( this.displayName, this.skills,);
 
 }
