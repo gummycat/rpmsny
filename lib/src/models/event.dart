df5fc6c134af896ed045769af67b7b8a30976938
Michael Jones <jones.mej@gmail.com>
2019-06-01 16:52:43 -0400

    Search updates with data on click

--- lib/src/models/event.dart
+++ lib/src/models/event.dart
@@ -16,23 +16,18 @@ class Event {
   //Constructor
   Event(this.eventKey, this.host, this.mentee, this.location, this.range, this .startTime, this.endTime);
 
- static List<Event> listEvents(){
-
-  //List<Event> events = new List();
-  //events.add(new Event(101, 'Evan Sanderson', 'Sam Evans', 'Washington DC,20', 5 ));
-  return[
-    Event('101', Host('Evan Sanderson','Java'), '', 'Washington DC', 20, DateTime.now(), DateTime.now()),
-    Event('102', Host('Steve Clark','Flutter'), '', 'New York, NY', 30, DateTime.now(), DateTime.now()),
-    Event('103', Host('Clark Johnson','Flutter'), '', 'Washington, DC', 50, DateTime.now(), DateTime.now()),
-    Event('104', Host('Jessica Rodgers', 'Flutter'),'', 'New York, NY', 60, DateTime.now(), DateTime.now()),
-    Event('105', Host('Shannon Adams', 'JavaScript'), '', 'Washington, DC', 10, DateTime.now(), DateTime.now()),
-    Event('106', Host('Peter Rabbit', 'Dart'), '', 'New York, NY', 20, DateTime.now(), DateTime.now()),
-    Event('107', Host('Collin Patterson','React'), '', 'Washington, DC', 20, DateTime.now(), DateTime.now()),
-    Event('108', Host('Lisa Hughes', 'React'), '', 'Richmond, VA', 25, DateTime.now(), DateTime.now()),
-    Event('109', Host('John Fredrickson', 'Cobol'), '', 'Richmond, VA', 30, DateTime.now(), DateTime.now()),
-    Event('100', Host('Fredrick Lewis', 'Perl'), '', 'Richmond, VA', 30, DateTime.now(), DateTime.now()),
-  ];
-
-
-}
+  static List<Event> listEvents() {
+    return [
+      Event('101', Host('Evan Sanderson','Java'), '', 'Washington DC', 20, DateTime.now(), DateTime.now()),
+      Event('102', Host('Steve Clark','Flutter'), '', 'New York, NY', 30, DateTime.now(), DateTime.now()),
+      Event('103', Host('Clark Johnson','Flutter'), '', 'Washington, DC', 50, DateTime.now(), DateTime.now()),
+      Event('104', Host('Jessica Rodgers', 'Flutter'),'', 'New York, NY', 60, DateTime.now(), DateTime.now()),
+      Event('105', Host('Shannon Adams', 'JavaScript'), '', 'Washington, DC', 10, DateTime.now(), DateTime.now()),
+      Event('106', Host('Peter Rabbit', 'Dart'), '', 'New York, NY', 20, DateTime.now(), DateTime.now()),
+      Event('107', Host('Collin Patterson','React'), '', 'Washington, DC', 20, DateTime.now(), DateTime.now()),
+      Event('108', Host('Lisa Hughes', 'React'), '', 'Richmond, VA', 25, DateTime.now(), DateTime.now()),
+      Event('109', Host('John Fredrickson', 'Cobol'), '', 'Richmond, VA', 30, DateTime.now(), DateTime.now()),
+      Event('100', Host('Fredrick Lewis', 'Perl'), '', 'Richmond, VA', 30, DateTime.now(), DateTime.now()),
+    ];
+  }
 }
--- lib/src/screens/search_screen.dart
+++ lib/src/screens/search_screen.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 import '../blocs/timer_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import '../models/event.dart';
+import '../models/host.dart';
 
 class SearchScreen extends StatefulWidget {
-  
   SearchScreen({Key key}) : super(key: key);
 
   _SearchScreenState createState() => _SearchScreenState();
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-
   _SearchScreenState();
   DateTime selectedDate = DateTime.now();
   final fmt = DateFormat('EEEE, MMMM d, y');
+  final fmtTime = DateFormat('h:m');
+  var events = List<Event>();
 
   Future<Null> _selectDate(BuildContext context) async {
     DateTime now = DateTime.now();
@@ -75,7 +77,27 @@ class _SearchScreenState extends State<SearchScreen> {
               Center(
                 child: RaisedButton(
                   child: Text('Search'),
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() { 
+                      _setSearchResults();
+                    });
+                  },
+                ),
+              ),
+              Flexible(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: events.length,
+                  itemBuilder: (contezt, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Theme.of(context).accentColor,
+                        child: Text(events[index].range.toString()),
+                      ),
+                      title: Text(events[index].host.displayName),
+                      subtitle: Text(_getEventSubtitle(events[index])),
+                    );
+                  },
                 ),
               )
             ],
@@ -83,5 +105,27 @@ class _SearchScreenState extends State<SearchScreen> {
         )
       ),
     );
+  }
+
+  String _getEventSubtitle(Event event) {
+    String start = fmtTime.format(event.startTime);
+    String end = fmtTime.format(event.endTime);
+
+    return '$start - $end';
+  }
+
+  void _setSearchResults() {
+    events = [
+      Event('101', Host('Evan Sanderson','Java'), '', 'Washington DC', 20, DateTime.now(), DateTime.now()),
+      Event('102', Host('Steve Clark','Flutter'), '', 'New York, NY', 30, DateTime.now(), DateTime.now()),
+      Event('103', Host('Clark Johnson','Flutter'), '', 'Washington, DC', 50, DateTime.now(), DateTime.now()),
+      Event('104', Host('Jessica Rodgers', 'Flutter'),'', 'New York, NY', 60, DateTime.now(), DateTime.now()),
+      Event('105', Host('Shannon Adams', 'JavaScript'), '', 'Washington, DC', 10, DateTime.now(), DateTime.now()),
+      Event('106', Host('Peter Rabbit', 'Dart'), '', 'New York, NY', 20, DateTime.now(), DateTime.now()),
+      Event('107', Host('Collin Patterson','React'), '', 'Washington, DC', 20, DateTime.now(), DateTime.now()),
+      Event('108', Host('Lisa Hughes', 'React'), '', 'Richmond, VA', 25, DateTime.now(), DateTime.now()),
+      Event('109', Host('John Fredrickson', 'Cobol'), '', 'Richmond, VA', 30, DateTime.now(), DateTime.now()),
+      Event('100', Host('Fredrick Lewis', 'Perl'), '', 'Richmond, VA', 30, DateTime.now(), DateTime.now()),
+    ];
   }
 }
