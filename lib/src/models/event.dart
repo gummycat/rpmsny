ffede19ce29e8422062423199ac30554c1c80a78
KraheZ4Code <sllkrahe@gmail.com>
2019-06-01 15:44:02 -0400

    SLK: Adding models for host and events

---
+++ lib/src/models/event.dart
@@ -0,0 +1,19 @@
+class Event {
+
+  final String eventKey;
+  final String host;
+  final String mentee;
+  final String location;
+  final String range;
+  final DateTime startTime;
+  final DateTime endTime;
+
+
+  //Constructor
+  Event(this.eventKey, this.host, this.mentee, this.location, this.range, this .startTime, this.endTime);
+
+
+
+
+
+}
